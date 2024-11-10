119d5565f083baf412a860c35ff76b7fe5e341d4
nkch22 <n.kriachko@proton.me>
2024-11-10 22:22:06 +0200

    Add queue data structure

--- include/queue.h
+++ include/queue.h
@@ -5,4 +5,16 @@
 #ifndef QUEUE_H
 #define QUEUE_H
 
+typedef struct queue_t queue;
+
+queue *queue_create_default();
+
+void queue_push(queue *queue, int item);
+
+int queue_pop(queue *queue);
+
+void queue_dealloc(queue *queue);
+
+void queue_print(queue *queue);
+
 #endif //QUEUE_H
--- src/queue/queue.c
+++ src/queue/queue.c
@@ -1,3 +1,37 @@
 //
 // Created by kurduli on 11/10/24.
 //
+
+#include "queue.h"
+
+#include <stdlib.h>
+
+#include "linked-list.h"
+
+struct queue_t {
+    linked_list *list;
+};
+
+queue *queue_create_default() {
+    queue *queue = malloc(sizeof *queue);
+    queue->list = list_create_default();
+    return queue;
+}
+
+void queue_push(queue *queue, int data) {
+    list_push_back(queue->list, data);
+}
+
+int queue_pop(queue *queue) {
+    return list_pop_front(queue->list);
+}
+
+void queue_dealloc(queue *queue) {
+    list_dealloc(queue->list);
+    free(queue);
+}
+
+void queue_print(queue *queue) {
+    list_print(queue->list);
+}
+
